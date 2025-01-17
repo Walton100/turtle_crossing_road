0dcbdb03e41e95a213e557a2c67c278a8264b35c
Emmanuel Walton <waltonelijah51@gmail.com>
2025-01-17 23:02:43 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -29,7 +29,11 @@ while game_is_on:
             game_is_on = False
             scoreboard.game_over()
 
-
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
 
 
 screen.exitonclick()
